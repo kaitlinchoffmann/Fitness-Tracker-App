62db12763d901b64096c31099dfb14d2c7eb123f
Kaitlin <kaitlinchoffmann@gmail.com>
2020-03-12 21:14:16 -0400

    Add to About page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div>
+  <div class="container about"><br/>
+      <h1 class="block title is-2">
+       What is Healthy Habits?
+      </h1><br/>
+    </div>
+    <div><br/>
+      <img src="../assets/food.png" alt="Healthy Food" class="my-image">
+    </div><br/>
+    <h5 class="title is-5 has-text-centered">
+      With Healthy Habits, users can keep a log of what they eat and any exercises done throughout the day. Whether you are trying to lose
+      weight, gain muscle, eat better or just keep yourself on track, Healthy Habits can help you achieve your goals. Sign up today and 
+      start tracking now!
+    </h5>
+    <div style="padding-bottom: 50px;"></div>
+  <footer class="footer"><div class="content has-text-centered">
+    A Web App by Kaitlin Hoffmann
+    </div></footer> 
+ </div> 
 </template>
+
+<style scoped>
+  .my-image {
+    width: 200px;
+  }
+  h5 {
+    color: slateblue;
+    font-weight: bold;
+    max-width: 500px;
+    margin: auto;
+    padding: 20px;
+    line-height: 30px;
+  }
+</style>
+
